b3b4ff7442e120c6e33ae17d9078a73dd3b2b744
dissolvedLS <deodex@spaces.ru>
2019-05-28 13:54:49 +0300

    Добавьте файлы проекта.

---
+++ prog-lab4-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace prog_lab4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader MyFile = new StreamReader("microsoft.txt");
+            string Text = MyFile.ReadToEnd();
+            Console.WriteLine("all text is:\n" + Text); 
+            string[] words = Text.Split(new Char[] { ' ', ',', '.', '(', ')' });
+            char LastLetter = words[0][ (words[0].Length-1) ];
+            int Count = 0;
+            foreach (string a in words)
+            {
+            if (a.Length!=0 && a[a.Length - 1] == LastLetter)
+                {
+                    Console.WriteLine(a);
+                    Count++;
+                }
+            }
+            Console.WriteLine("result is " + Count + " words");
+        }
+    }
+}
